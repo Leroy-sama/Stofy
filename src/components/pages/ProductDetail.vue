38b12a46d2a0ff881e90d56a72a883d3c8f0def4
Leroy <ntruderleroy@gmail.com>
2024-08-14 18:06:16 +0300

    use fake store API

--- src/components/pages/ProductDetail.vue
+++ src/components/pages/ProductDetail.vue
@@ -1,268 +1,247 @@
 <template>
-    <section class="details" v-if="selectedProduct">
-        <div class="firstSection">
-            <div class="image__details">
-                <img class="main-img" :src="mainImage" :alt="title" />
-                <div class="small-imgs">
-                    <div
-                        class="box"
-                        v-for="(
-                            images, index
-                        ) in selectedProduct.secondaryImages"
-                        :key="index"
-                        @click="updateMainImage(images)"
-                    >
-                        <img class="box-imgs" :src="images" />
-                    </div>
-                </div>
-            </div>
-            <div class="description__details">
-                <span class="brand">{{ brand }}</span>
-                <h2 class="desc__head">{{ title }}</h2>
-                <p class="desc__text">
-                    {{ description }}
-                </p>
-                <span class="price">${{ price }}</span>
-                <div class="cartQuantity">
-                    <QuantityItem :quantity="quantity" />
-                    <button class="cart-btn" @click="addToCart">
-                        Add to Cart
-                    </button>
-                </div>
-            </div>
-        </div>
-        <div class="description">
-            <h1 class="desc__head">Description</h1>
-            <p class="desc__text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
-                repellat, sequi adipisci labore, debitis consequatur expedita
-                eius sed dolores explicabo et, cum excepturi facere provident
-                architecto dolore quibusdam! Veritatis, quasi?
-            </p>
-            <span class="properties">Properties</span>
-            <ul class="properties__list">
-                <li>Lorem ipsum</li>
-                <li>Lorem ipsum</li>
-                <li>Lorem ipsum</li>
-                <li>Lorem ipsum</li>
-                <li>Lorem ipsum</li>
-                <li>Lorem ipsum</li>
-            </ul>
-        </div>
-        <div class="mayLike">
-            <h2 class="maylike__head">You may also like</h2>
-            <div class="mayLike__products">
-                <div class="product">
-                    <img src="@/assets/images/hdmVans.jpg" />
-                    <span class="product__head">Product 1</span>
-                </div>
-                <div class="product">
-                    <img src="@/assets/images/yeezy.jpeg" />
-                    <span class="product__head">Product 2</span>
-                </div>
-                <div class="product">
-                    <img src="@/assets/images/nikeTravisScott.jpg" />
-                    <span class="product__head">Product 3</span>
-                </div>
-            </div>
-        </div>
-    </section>
-    <section v-else>
-        <h1>Loading loading loading....</h1>
-    </section>
+	<section class="details" v-if="product">
+		<div class="firstSection">
+			<div class="image__details">
+				<img class="main-img" :src="image" :alt="title" />
+			</div>
+			<div class="description__details">
+				<span class="brand">{{ category }}</span>
+				<h2 class="desc__head">{{ title }}</h2>
+				<p class="desc__text">
+					{{ description }}
+				</p>
+				<span class="price">${{ price }}</span>
+				<div class="cartQuantity">
+					<QuantityItem :quantity="quantity" />
+					<button class="cart-btn" @click="addToCart">
+						Add to Cart
+					</button>
+				</div>
+			</div>
+		</div>
+		<div class="description">
+			<h1 class="desc__head">Description</h1>
+			<p class="desc__text">
+				{{ description }}
+			</p>
+		</div>
+		<div class="mayLike">
+			<h2 class="maylike__head">You may also like</h2>
+			<div class="mayLike__products">
+				<div class="product">
+					<img
+						src="https://i.pinimg.com/564x/47/cd/3a/47cd3a7466fed5fb38135049c7dae06b.jpg"
+					/>
+					<span class="product__head">Product 1</span>
+				</div>
+				<div class="product">
+					<img
+						src="https://i.pinimg.com/564x/36/cd/09/36cd09baefff367ff94cdeae9b59fc2d.jpg"
+					/>
+					<span class="product__head">Product 2</span>
+				</div>
+				<div class="product">
+					<img
+						src="https://i.pinimg.com/564x/b0/5d/6d/b05d6da86ffe1ed6c73df588ca7fefd5.jpg"
+					/>
+					<span class="product__head">Product 3</span>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section v-else>
+		<h1>Loading loading loading....</h1>
+	</section>
 </template>
 
 <script setup>
-    import QuantityItem from "@/components/items/QuantityItem.vue";
+	import QuantityItem from "@/components/items/QuantityItem.vue";
 
-    import { useProductStore } from "@/pinia/productStore";
-    import { useCartStore } from "@/pinia/cartStore";
-    import { onMounted, computed, ref } from "vue";
-    import { useRoute } from "vue-router";
+	import { useCartStore } from "@/pinia/cartStore";
+	import { onMounted, computed, ref } from "vue";
+	import { useRoute } from "vue-router";
 
-    const productStore = useProductStore();
-    const cartStore = useCartStore();
+	const cartStore = useCartStore();
 
-    const route = useRoute();
+	const route = useRoute();
 
-    const id = route.params.id;
+	const product = ref(null);
 
-    const selectedProduct = ref(null);
-    const quantity = ref(0);
+	const id = route.params.id;
 
-    const title = computed(() => {
-        return selectedProduct.value.title;
-    });
+	const quantity = ref(0);
 
-    const description = computed(() => {
-        return selectedProduct.value.description;
-    });
+	const title = computed(() => {
+		return product.value.title;
+	});
 
-    const price = computed(() => {
-        return selectedProduct.value.price;
-    });
+	const description = computed(() => {
+		return product.value.description;
+	});
 
-    const mainImage = computed(() => {
-        return mainImageSrc.value || selectedProduct.value.mainImage;
-    });
+	const price = computed(() => {
+		return product.value.price;
+	});
 
-    const brand = computed(() => {
-        return selectedProduct.value.brand;
-    });
+	const image = computed(() => {
+		return product.value.image;
+	});
 
-    onMounted(() => {
-        const products = productStore.products;
-        selectedProduct.value = products.find((product) => {
-            return product.id === id;
-        });
-    });
+	const category = computed(() => {
+		return product.value.category;
+	});
 
-    const mainImageSrc = ref(null);
+	const getProductData = async (id) => {
+		const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+		const data = await response.json();
+		console.log(data);
+		product.value = data;
+	};
 
-    const updateMainImage = (images) => {
-        mainImageSrc.value = images;
-    };
+	onMounted(() => {
+		getProductData(route.params.id);
+	});
 
-    const addToCart = () => {
-        cartStore.addProductToCart({
-            id: selectedProduct.value.id,
-            title: selectedProduct.value.title,
-            mainImage: selectedProduct.value.mainImage,
-            price: selectedProduct.value.price,
-            qty: selectedQuantity.value,
-        });
-    };
+	const addToCart = () => {
+		cartStore.addProductToCart({
+			id: product.value.id,
+			title: product.value.title,
+			mainImage: product.value.mainImage,
+			price: product.value.price,
+			qty: selectedQuantity.value,
+		});
+	};
 </script>
 
 <style scoped>
-    li {
-        list-style: none;
-    }
+	li {
+		list-style: none;
+	}
 
-    .details {
-        max-width: 1200px;
-        margin-inline: auto;
-        padding: 2em 0;
-    }
+	.details {
+		max-width: 1200px;
+		margin-inline: auto;
+		padding: 2em 0;
+	}
 
-    .firstSection {
-        display: grid;
-        gap: 3em;
+	.firstSection {
+		display: grid;
+		gap: 3em;
 
-        padding: 2em 0;
-    }
+		padding: 2em 0;
+	}
 
-    .main-img {
-        inline-size: 100%;
-        aspect-ratio: 4/3;
-        object-fit: cover;
-    }
+	.main-img {
+		inline-size: 100%;
+		/* aspect-ratio: 4/3;
+		object-fit: cover; */
+	}
 
-    .box {
-        padding: 1em 0;
-    }
+	.box {
+		padding: 1em 0;
+	}
 
-    .box-imgs {
-        /* max-width: 300px; */
-        inline-size: 100%;
-        aspect-ratio: 4/3;
-        object-fit: cover;
-    }
+	.box-imgs {
+		/* max-width: 300px; */
+		inline-size: 100%;
+		aspect-ratio: 4/3;
+		object-fit: cover;
+	}
 
-    .small-imgs {
-        display: flex;
-        gap: 1em;
-    }
+	.small-imgs {
+		display: flex;
+		gap: 1em;
+	}
 
-    .brand {
-        text-transform: uppercase;
-        font-weight: 700;
-    }
+	.brand {
+		text-transform: uppercase;
+		font-weight: 700;
+	}
 
-    .desc__head {
-        font-size: 2rem;
-    }
+	.desc__head {
+		font-size: 2rem;
+	}
 
-    .desc__text {
-        padding: 1em 0;
-    }
+	.desc__text {
+		padding: 1em 0;
+	}
 
-    .price {
-        font-family: "Fraunces", serif;
-        font-size: 1.5rem;
-    }
+	.price {
+		font-family: "Fraunces", serif;
+		font-size: 1.5rem;
+	}
 
-    .description {
-        padding: 2em 0;
-    }
+	.description {
+		padding: 2em 0;
+	}
 
-    .cartQuantity {
-        display: flex;
-        gap: 1.5em;
-        flex-direction: column;
+	.cartQuantity {
+		display: flex;
+		gap: 1.5em;
+		flex-direction: column;
 
-        margin: 1.5em 0;
-    }
+		margin: 1.5em 0;
+	}
 
-    .cartQuantity .cart-btn {
-        padding: 1em 3em;
-        background-color: black;
-        color: white;
-        border: 1px solid black;
-        cursor: pointer;
-        width: 100%;
-    }
+	.cartQuantity .cart-btn {
+		padding: 1em 3em;
+		background-color: black;
+		color: white;
+		border: 1px solid black;
+		cursor: pointer;
+		width: 100%;
+	}
 
-    .cartQuantity .cart-btn:hover {
-        color: black;
-        background-color: white;
-    }
+	.cartQuantity .cart-btn:hover {
+		color: black;
+		background-color: white;
+	}
 
-    .properties {
-        font-weight: 700;
-    }
+	.properties {
+		font-weight: 700;
+	}
 
-    .mayLike {
-        padding: 2em 0;
-    }
+	.mayLike {
+		padding: 2em 0;
+	}
 
-    .mayLike__products {
-        display: grid;
-        gap: 1em;
-    }
+	.mayLike__products {
+		display: grid;
+		gap: 1em;
+	}
 
-    .product img {
-        inline-size: 100%;
-        aspect-ratio: 4/3;
-        object-fit: cover;
-    }
+	.product img {
+		inline-size: 100%;
+		aspect-ratio: 4/3;
+		object-fit: cover;
+	}
 
-    .details {
-        padding: 0 1em;
-    }
+	.details {
+		padding: 0 1em;
+	}
 
-    @media (min-width: 35em) {
-        .firstSection {
-            grid-template-columns: 1fr 1fr;
-        }
+	@media (min-width: 35em) {
+		.firstSection {
+			grid-template-columns: 1fr 1fr;
+		}
 
-        .cartQuantity {
-            display: flex;
-            flex-direction: row;
-            justify-content: space-between;
-            align-items: center;
-        }
+		.cartQuantity {
+			display: flex;
+			flex-direction: row;
+			justify-content: space-between;
+			align-items: center;
+		}
 
-        .cartQuantity .cart-btn {
-            width: auto;
-        }
+		.cartQuantity .cart-btn {
+			width: auto;
+		}
 
-        .mayLike__products {
-            grid-template-columns: repeat(3, 1fr);
-        }
+		.mayLike__products {
+			grid-template-columns: repeat(3, 1fr);
+		}
 
-        .desc__head {
-            font-size: 3rem;
-        }
-    }
+		.desc__head {
+			font-size: 3rem;
+		}
+	}
 </style>
--- src/components/pages/ProductsPage.vue
+++ src/components/pages/ProductsPage.vue
@@ -1,112 +1,118 @@
 <template>
-    <section class="products">
-        <div class="products__wrapper">
-            <div class="head">
-                <div class="head__head">
-                    <span class="line"></span>
-                    <h1 class="products__head">All Products</h1>
-                </div>
-                <div class="filter">
-                    <h3>Filter:</h3>
-                </div>
-            </div>
-            <div class="search">
-                <div class="form__control">
-                    <input type="text" placeholder="Search products..." />
-                </div>
-            </div>
-            <div class="productitems__wrapper">
-                <ProductItem
-                    v-for="prod in productStore.products"
-                    :key="prod.id"
-                    :id="prod.id"
-                    :title="prod.title"
-                    :mainImage="prod.mainImage"
-                    :description="prod.description"
-                    :price="prod.price"
-                >
-                </ProductItem>
-            </div>
-        </div>
-    </section>
+	<section class="products">
+		<div class="products__wrapper">
+			<div class="head">
+				<div class="head__head">
+					<span class="line"></span>
+					<h1 class="products__head">All Products</h1>
+				</div>
+				<div class="filter">
+					<h3>Filter:</h3>
+				</div>
+			</div>
+			<div class="search">
+				<div class="form__control">
+					<input type="text" placeholder="Search products..." />
+				</div>
+			</div>
+			<div class="productitems__wrapper">
+				<ProductItem
+					v-for="prod in products"
+					:key="prod.id"
+					:id="prod.id"
+					:title="prod.title"
+					:mainImage="prod.image"
+					:price="prod.price"
+				>
+				</ProductItem>
+			</div>
+		</div>
+	</section>
 </template>
 
 <script setup>
-    import { useProductStore } from "@/pinia/productStore";
-    import ProductItem from "../items/ProductItem.vue";
+	import ProductItem from "../items/ProductItem.vue";
+	import { onMounted, ref } from "vue";
 
-    const productStore = useProductStore();
+	const products = ref([]);
 
-    // const products = computed(() => {
-    //     return store.getters["prods/products"];
-    // });
+	const getProducts = async () => {
+		const response = await fetch("https://fakestoreapi.com/products");
+		const data = await response.json();
+		console.log(data);
+		products.value = data;
+	};
+
+	onMounted(() => {
+		getProducts();
+	});
 </script>
 
 <style scoped>
-    .products {
-        margin: 0 1em 3em;
-    }
+	.products {
+		margin: 0 1em 3em;
+	}
 
-    .head__head {
-        display: flex;
-        align-items: center;
-        gap: 1em;
-    }
+	.head__head {
+		display: flex;
+		align-items: center;
+		gap: 1em;
+	}
 
-    .line {
-        background-color: rgb(5, 31, 5);
-        display: block;
-        width: 50px;
-        height: 2px;
-    }
+	.line {
+		background-color: rgb(5, 31, 5);
+		display: block;
+		width: 50px;
+		height: 2px;
+	}
 
-    .products__head {
-        color: rgb(5, 31, 5);
-        padding: 1em 0;
-    }
+	.products__head {
+		color: rgb(5, 31, 5);
+		padding: 1em 0;
+	}
 
-    .productitems__wrapper {
-        display: grid;
-        gap: 2em;
-    }
+	.productitems__wrapper {
+		display: grid;
+		gap: 2em;
+	}
 
-    .search {
-        display: flex;
-        justify-content: center;
-    }
+	.search {
+		display: flex;
+		justify-content: center;
+	}
 
-    .form__control {
-        padding: 2em;
-    }
+	.form__control {
+		padding: 2em;
+	}
 
-    .form__control input {
-        max-width: 100%;
-        width: 600px;
-        padding: 0.5em 1em;
-        outline: none;
-    }
+	.form__control input {
+		max-width: 100%;
+		width: 600px;
+		padding: 0.5em 1em;
+		outline: none;
+	}
 
-    .head {
-        display: flex;
-        flex-direction: column;
-        gap: 1em;
-    }
+	.head {
+		display: flex;
+		flex-direction: column;
+		gap: 1em;
+	}
 
-    @media (min-width: 35em) {
-        .products__wrapper {
-            max-width: 1200px;
-            margin-inline: auto;
-        }
+	@media (min-width: 35em) {
+		.products__wrapper {
+			max-width: 1200px;
+			margin-inline: auto;
+		}
 
-        .productitems__wrapper {
-            grid-template-columns: repeat(3, 1fr);
-            gap: 2em;
-        }
+		.productitems__wrapper {
+			grid-template-columns: repeat(3, 1fr);
+			gap: 2em;
+		}
 
-        .head {
-            flex-direction: row;
-            justify-content: space-between;
-            align-items: center;
-        }
-    }
+		.head {
+			flex-direction: row;
+			justify-content: space-between;
+			align-items: center;
+		}
+	}
 </style>
