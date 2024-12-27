83228252fe0d77e76a06f419b910b3023bf3deb1
Leroy <ntruderleroy@gmail.com>
2024-12-27 15:56:42 +0300

    the footer

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+	import TheFooter from "./layout/TheFooter.vue";
 	import TheHeader from "./layout/TheHeader.vue";
 </script>
 
 <template>
 	<TheHeader />
 	<RouterView />
+	<TheFooter />
 </template>
